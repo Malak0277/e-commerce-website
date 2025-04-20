d792fc1ea1959da4621aad70e299d3ebef102115
Malak Malak <malaksalem2003@gmail.com>
2025-04-20 16:34:17 +0200

    setting up backend

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cakeRouter = require('./routes/cakes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/api/cakes', cakeRouter);
+
+
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/public/html/home.html');
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
